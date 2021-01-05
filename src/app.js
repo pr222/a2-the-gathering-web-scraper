780ba66ca2f4ac2145b8d3f2bb39cd269d946d6e
Pauliina Raitaniemi <pr222ja@student.lnu.se>
2021-01-05 14:41:20 +0100

    Handle incoming arguments

--- src/app.js
+++ src/app.js
@@ -4,4 +4,26 @@
  * @author Pauliina Raitaniemi <pr222ja@student.lnu.se>
  * @version 1.0.0
  */
-console.log('The app ran')
+import validator from 'validator'
+
+/**
+ * Wrapper for running the main application asyncronously.
+ */
+const wrapper = async () => {
+  try {
+    // Only use the first custom user-provided commando line
+    // starting argument, skipping the two first standard ones.
+    const [argument] = process.argv.slice(2)
+
+    // Make sure the argument is an URL in the first place.
+    if (!validator.isURL(argument)) {
+      throw new Error('The provided argument was not an URL.')
+    }
+    console.log(argument)
+    console.log('The app ran to completion.')
+  } catch (err) {
+    console.error(err)
+  }
+}
+
+wrapper()
