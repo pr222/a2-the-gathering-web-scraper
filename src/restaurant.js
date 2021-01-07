e4f435377bb420e2677c4bbe8a637f25036fa933
Pauliina Raitaniemi <pr222ja@student.lnu.se>
2021-01-07 17:45:21 +0100

    Add constructor and getter to Restaurant

--- src/restaurant.js
+++ src/restaurant.js
@@ -5,3 +5,41 @@
  * @version 1.0.0
  */
 
+/**
+ * Class for finding free tables in a restaurant.
+ *
+ * @exports
+ * @class Restaurant
+ */
+export class Restaurant {
+/**
+ * Creates an instance of Restaurant.
+ *
+ * @memberof Restaurant
+ */
+  constructor () {
+    this._freeTables = []
+  }
+
+  /**
+   * Get the free tables.
+   *
+   * @readonly
+   * @memberof Restaurant
+   * @returns {object[]} - Free tables.
+   */
+  get freeTables () {
+    return [...this._freeTables]
+  }
+
+  /**
+   * Find suggestions for tables to book after movies.
+   *
+   * @param {string} url - to the restaurant's login page.
+   * @param {object[]} movies - to watch before eating.
+   * @memberof Restaurant
+   */
+  async findTableSuggestions (url, movies) {
+
+  }
+}
