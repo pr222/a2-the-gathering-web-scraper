5c6b3a275d6a7ef055e802074b13b567473fdef7
Pauliina Raitaniemi <pr222ja@student.lnu.se>
2021-01-08 01:11:29 +0100

    Match movies and table times

--- src/compose-suggestions.js
+++ src/compose-suggestions.js
@@ -18,7 +18,24 @@ export function composeSuggestion (movies, tables) {
   let day, movieTitle, movieTime, tableStart, tableEnd
   console.log(movies)
   console.log(tables)
+  const suggestions = []
 
+  for (const movie of movies) {
+    console.log(movie.time)
+    const movieDuration = `${parseInt(movie.time.slice(0, 2)) + 2}`
+    console.log(movieDuration)
+    console.log('-------------------------------------')
+    // Handle information for each movie.
+    for (const table of tables) {
+      if (movie.day === table.day) {
+        if (movieDuration === table.tableStart) {
+          console.log('MATCH!')
+          console.log(movie)
+          console.log(table)
+        }
+      }
+    }
+  }
 
   return `* On ${day}, "${movieTitle}" begins at ${movieTime}:00, and there is a free table to book between ${tableStart}:00-${tableEnd}:00.`
 }
